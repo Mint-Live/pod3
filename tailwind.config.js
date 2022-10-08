7921929668c425d5f2940348be586a9431946106
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-08 15:06:00 -0500

    feat: welcome screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto Mono", "monospace"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
